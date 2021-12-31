4ba27d32e151516b84720b98c981c18f5c2cbbc8
tanhtanh1505 <tanhtanh1505@gmail.com>
2021-12-31 22:33:04 +0700

    say hi!

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace ToolsAF
+{
+    partial class TAF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button1.Location = new System.Drawing.Point(93, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(101, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Auto Fishing";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button2.Location = new System.Drawing.Point(93, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button3.Location = new System.Drawing.Point(81, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 24);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Test";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.radioButton1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.radioButton1.Location = new System.Drawing.Point(12, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(72, 21);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "waiting";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.checkBox1.Location = new System.Drawing.Point(41, 99);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(100, 21);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Auto repair";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.checkBox2.Location = new System.Drawing.Point(166, 99);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(84, 21);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "Any Rod";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // TAF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 278);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Name = "TAF";
+            this.Text = "ToolAF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using KAutoHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace ToolsAF
+{
+    public partial class TAF : Form
+    {
+        public TAF()
+        {
+            InitializeComponent();
+        }
+
+        public bool CompareBitmapsFast(Bitmap bmp1, Bitmap bmp2)
+        {
+            if (bmp1 == null || bmp2 == null)
+                return false;
+            if (object.Equals(bmp1, bmp2))
+                return true;
+            if (!bmp1.Size.Equals(bmp2.Size) || !bmp1.PixelFormat.Equals(bmp2.PixelFormat))
+                return false;
+
+            int bytes = bmp1.Width * bmp1.Height * (Image.GetPixelFormatSize(bmp1.PixelFormat) / 8);
+
+            bool result = true;
+            byte[] b1bytes = new byte[bytes];
+            byte[] b2bytes = new byte[bytes];
+
+            BitmapData bitmapData1 = bmp1.LockBits(new Rectangle(0, 0, bmp1.Width, bmp1.Height), ImageLockMode.ReadOnly, bmp1.PixelFormat);
+            BitmapData bitmapData2 = bmp2.LockBits(new Rectangle(0, 0, bmp2.Width, bmp2.Height), ImageLockMode.ReadOnly, bmp2.PixelFormat);
+
+            Marshal.Copy(bitmapData1.Scan0, b1bytes, 0, bytes);
+            Marshal.Copy(bitmapData2.Scan0, b2bytes, 0, bytes);
+
+            for (int n = 0; n <= bytes - 1; n++)
+            {
+                if (b1bytes[n] - b2bytes[n] > 10 || b1bytes[n] - b2bytes[n] < -10)
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            bmp1.UnlockBits(bitmapData1);
+            bmp2.UnlockBits(bitmapData2);
+
+            return result;
+        }
+        public Bitmap MakeGrayscale(Bitmap original)
+        {
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+            Graphics g = Graphics.FromImage(newBitmap);
+
+
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][]
+               {
+           new float[] {.3f, .3f, .3f, 0, 0},
+           new float[] {.59f, .59f, .59f, 0, 0},
+           new float[] {.11f, .11f, .11f, 0, 0},
+           new float[] {0, 0, 0, 1, 0},
+           new float[] {0, 0, 0, 0, 1}
+               });
+
+            ImageAttributes attributes = new ImageAttributes();
+            attributes.SetColorMatrix(colorMatrix);
+
+            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+
+            g.Dispose();
+            return newBitmap;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var handle = AutoControl.FindWindowHandle("Qt5QWindowIcon" == "" ? null : "Qt5QWindowIcon", "" == "" ? null : "");
+            bool rod = false;
+        LOOP:
+            label1.Text = "Running...";
+            Task.Delay(4000).Wait();
+            Bitmap bmp = (Bitmap)CaptureHelper.CaptureWindow(handle);
+            bmp = CaptureHelper.CropImage(bmp, new System.Drawing.Rectangle(780, 190, 40, 30));
+            bmp = MakeGrayscale(bmp);
+            for (int i = 0; i < 10000; i++)
+            {
+                Task.Delay(10).Wait();
+                Bitmap bmpc = (Bitmap)CaptureHelper.CaptureWindow(handle);
+                bmpc = CaptureHelper.CropImage(bmpc, new System.Drawing.Rectangle(780, 190, 40, 30));
+                bmpc = MakeGrayscale(bmpc);
+                if (CompareBitmapsFast(bmp, bmpc))
+                {
+                    label1.Text = "Waiting...";
+                }
+                else
+                {
+                    label1.Text = "Done!";
+                    //AutoControl.SendKeyFocus(KeyCode.SPACE_BAR);
+                    AutoControl.SendKeyBoardPress(handle, VKeys.VK_SPACE);
+                    break;
+                }
+
+            }
+            Task.Delay(5000).Wait();
+            //AutoControl.SendKeyFocus(KeyCode.KEY_S);
+            AutoControl.SendKeyBoardPress(handle, VKeys.VK_S);
+            Task.Delay(500).Wait();
+            //AutoControl.SendKeyFocus(KeyCode.KEY_S);
+            AutoControl.SendKeyBoardPress(handle, VKeys.VK_S);
+            Task.Delay(1000).Wait();
+            //AutoControl.SendKeyFocus(KeyCode.KEY_O);
+            AutoControl.SendKeyBoardPress(handle, VKeys.VK_O);
+            Task.Delay(1000).Wait();
+            //AutoControl.SendKeyFocus(KeyCode.KEY_3);
+            AutoControl.SendKeyBoardPress(handle, VKeys.VK_3);
+            Task.Delay(1000).Wait();
+            //AutoControl.SendKeyFocus(KeyCode.KEY_B);
+            if (rod)
+            {
+                AutoControl.SendKeyBoardPress(handle, VKeys.VK_B);
+                rod = false;
+            }
+            else
+            {
+                AutoControl.SendKeyBoardPress(handle, VKeys.VK_A);
+                rod = true;
+            }
+            Task.Delay(1000).Wait();
+            //AutoControl.SendKeyFocus(KeyCode.SHIFT);
+            AutoControl.SendKeyBoardPress(handle, VKeys.VK_SHIFT);
+            Task.Delay(1000).Wait();
+            goto LOOP;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Pause");
+            var handle = AutoControl.FindWindowHandle("Qt5QWindowIcon" == "" ? null : "Qt5QWindowIcon", "" == "" ? null : "");
+            Bitmap bmp = (Bitmap)CaptureHelper.CaptureWindow(handle);
+            bmp = CaptureHelper.CropImage(bmp, new System.Drawing.Rectangle(780, 190, 40, 30));
+            bmp.Save("test.png");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*Rectangle r = new Rectangle(800, 272,
+                                        325, 180);
+
+            
+        LOOP:
+            label1.Text = "Running...";
+            Task.Delay(4000).Wait();
+            Bitmap bmp = new Bitmap(r.Width, r.Height, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(r.Left, r.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+            bmp = MakeGrayscale(bmp);
+            for (int i = 0; i < 10000; i++)
+            {
+                Task.Delay(10).Wait();
+                Bitmap bmpc = new Bitmap(r.Width, r.Height, PixelFormat.Format32bppArgb);
+                Graphics gc = Graphics.FromImage(bmpc);
+                gc.CopyFromScreen(r.Left, r.Top, 0, 0, bmpc.Size, CopyPixelOperation.SourceCopy);
+                bmpc = MakeGrayscale(bmpc);
+                if (CompareBitmapsFast(bmp, bmpc))
+                {
+                    label1.Text = "Waiting...";
+                }
+                else
+                {
+                    label1.Text = "Done!";
+                    AutoControl.SendKeyFocus(KeyCode.SPACE_BAR);
+                    break;
+                }
+
+            }
+            Task.Delay(5000).Wait();
+            AutoControl.SendKeyFocus(KeyCode.KEY_S);
+            Task.Delay(500).Wait();
+            AutoControl.SendKeyFocus(KeyCode.KEY_S);
+            Task.Delay(1000).Wait();
+            AutoControl.SendKeyFocus(KeyCode.KEY_O);
+            Task.Delay(1000).Wait();
+            AutoControl.SendKeyFocus(KeyCode.KEY_3);
+            Task.Delay(1000).Wait();
+            AutoControl.SendKeyFocus(KeyCode.KEY_B);
+            Task.Delay(1000).Wait();
+            AutoControl.SendKeyFocus(KeyCode.SHIFT);
+            Task.Delay(1000).Wait();
+            goto LOOP;*/
+
+            var handle = AutoControl.FindWindowHandle("Qt5QWindowIcon" == "" ? null : "Qt5QWindowIcon", "" == "" ? null : "");
+            AutoControl.SendKeyBoardPress(handle, VKeys.VK_SPACE);
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Task.Delay(3000).Wait();
+            radioButton1.Checked = true;
+            radioButton1.Text = "Connected";
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+            radioButton1.Text = "Connected";
+        }
+    }
+}
